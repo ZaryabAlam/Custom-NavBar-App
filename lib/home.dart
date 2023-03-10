cd0c868f44cb71162800ca8350d9a130b1de2bfc
ZaryabAlam <zaryabalam97@gmail.com>
2023-03-11 00:00:05 +0500

    Project End

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,6 @@ import 'package:navbar2/views/page4.dart';
 import 'package:navbar2/views/page5.dart';
 import 'views/page1.dart';
 
-//test
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
